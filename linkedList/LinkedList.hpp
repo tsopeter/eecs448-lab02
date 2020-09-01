8f0b1d0d36f4a56676afe77ccdc93535adc76315
Tso, Peter <tsopeter@ku.edu>
2020-08-31 21:07:41 -0500

    -changes
    		++linkedList/LinkedList.hpp
    
    			-LinkedList<T>::search(T value)
    				added search functionality through incrementing through
    				linked list structure

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,10 +38,12 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
-
+	while(temp != nullptr){
+		if(value == temp->getValue()){
+			isFound = true;
+		}
+		temp = temp->getNex();
+	}
 	return(isFound);
 }
 
